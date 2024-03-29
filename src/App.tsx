bd0e329eb73a37c963e7673e5bb361b5e99b75bb
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-30 01:37:20 +0530

    Add event listener for key press

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { useEffect } from "react";
 import Content from "./components/Content";
+import { useRecoilState } from "recoil";
+import { contentStr } from "./state/contentState";
 
 function App() {
+    const [content, setContent] = useRecoilState(contentStr);
+
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (
+                /^[a-z]$/.test(event.key) ||
+                /^[A-Z]$/.test(event.key) ||
+                event.key === " "
+            ) {
+                console.log(event.key);
+                if (content.length && content[0] == event.key) {
+                    setContent((prevContent) => prevContent.slice(1));
+                }
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyPress);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+        };
+    }, [content]);
+
     return (
         <div className="p-5">
             <h1 className="text-3xl font-bold underline">
