37d38ae5af4a42f2d8a38b6c8675549a1d97c349
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-30 08:56:46 +0530

    Update timer style

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,7 +5,11 @@ function Timer() {
     const [decrementStarted] = useRecoilState(decrementState);
     const [timer] = useRecoilState(timerState);
 
-    return <div>{decrementStarted ? <h2>{timer}</h2> : <></>}</div>;
+    return (
+        <div className="my-3">
+            {decrementStarted ? <h2 className="text-2xl">{timer}</h2> : <></>}
+        </div>
+    );
 }
 
 export default Timer;
